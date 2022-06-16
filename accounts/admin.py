f0800d2196e0f467d236035837322d53a6ef8a99
tohidworld <90111960+tohidworld@users.noreply.github.com>
2022-06-16 18:29:16 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from accounts.models import User, Doctor, Patient
-
-admin.site.register(User)
-admin.site.register(Doctor)
-admin.site.register(Patient)
